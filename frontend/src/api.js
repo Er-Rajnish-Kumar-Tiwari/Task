d37d6c18a8cc6c061320fbe7d0aa9357b33694e2
Er-Rajnish-Kumar-Tiwari <tanish281202@gmail.com>
2025-06-20 20:32:55 +0100

    second

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 // api.js
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://task-d0eq.onrender.com' });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
